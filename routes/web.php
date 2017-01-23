7fd25944ca76539d5df5f35fb33a4472228b4525
Kenneth Fung <Qbit.Consulting@gmail.com>
2017-01-23 12:35:18 +1000

    Backend test: PHP Web Service Finalized
    API /api/findSmallestLeaf constructed as per specifications.
    Perform php-cs-fixer checking on web.php.

--- routes/web.php
+++ routes/web.php
@@ -32,15 +32,15 @@ $app->get('/api/library/{id}', function ($id) {
         $filesystem = new Filesystem($adapter);
         $content = $filesystem->read('save.log');
         $array_content = json_decode($content, true);
-        if (is_null($array_content)){
-          $result = response($v->messages(), 404); // Return Not Found response
+        if (is_null($array_content)) {
+            $result = response($v->messages(), 404); // Return Not Found response
         } else {
-          foreach ($array_content as $library) { // Search for the first array with matching id
+            foreach ($array_content as $library) { // Search for the first array with matching id
             if ($library['id'] && $library['id']==$id) {
                 $result = response()->json($library); // Assign library json to response
                 break;
             }
-          }
+            }
         }
     } else {
         $result = response($v->messages(), 404); // Return Not Found response
@@ -65,18 +65,86 @@ $app->post('/api/library', ['middleware'=>'auth', function (Request $request) {
   ]);
     if ($v->passes()) {
         // The request is valid, it's time to save Library data...
-    $library = $request->input('library');
-    $user = $request->user();
-    // Store json to local file
-    $root_dir = realpath('./../storage/logs');
-    $adapter = new Local($root_dir);
-    $filesystem = new Filesystem($adapter);
-    $content = ($filesystem->has('save.log')) ? $filesystem->read('save.log') : '';
-    $filesystem->put('save.log', $content.$library.PHP_EOL); // Store json string to local file. Use database in production for better performance.
-    Log::info('Authorized user saved library object: '.$library); // Logging
-    $result = response()->json(['Library json saved'=>$library]); // return json response
+        $library = $request->input('library');
+        $user = $request->user();
+        // Store json to local file
+        $root_dir = realpath('./../storage/logs');
+        $adapter = new Local($root_dir);
+        $filesystem = new Filesystem($adapter);
+        $content = ($filesystem->has('save.log')) ? $filesystem->read('save.log') : '';
+        $filesystem->put('save.log', $content.$library.PHP_EOL); // Store json string to local file. Use database in production for better performance.
+        Log::info('Authorized user saved library object: '.$library); // Logging
+        $result = response()->json(['Library json saved'=>$library]); // return json response
     } else {
         $result = response($v->messages(), 400); // return Bad Request response
     }
     return $result;
 }]);
+
+/* Support for /api/findSmallestLeaf */
+
+/* Debugging */
+$test_json = '{
+  "root": 1,
+  "left": {
+    "root": 7,
+    "left": {
+      "root": 2
+    },
+    "right": {
+      "root": 6
+    }
+  },
+  "right": {
+    "root": 5,
+    "left": {
+      "root": 9
+    }
+  }
+}';
+
+$test_obj = json_decode($test_json);
+/* End of Debugging */
+
+function findSmallestLeaf($obj)
+{
+    $sLeaf = null;
+    $candidates = findSmallerLeaves($obj);
+    $sLeaf = min($candidates);
+    return $sLeaf;
+}
+
+$list = array();
+
+function findSmallerLeaves($obj)
+{
+    global $list;
+    if (!isset($obj->left) && !isset($obj->right)) { //Cofirm as leaf if no left/right node existed
+        $list[] = $obj->root;
+    }
+    if (isset($obj->left)) { //Keep searching if left node existed
+        findSmallerLeaves($obj->left);
+    }
+    if (isset($obj->right)) { //Keep searching if right node existed
+        findSmallerLeaves($obj->right);
+    }
+    return $list;
+}
+/* End of Support for /api/findSmallestLeaf */
+
+//Debugging api
+$app->get('/api/test/findSmallestLeaf', function () use ($test_obj) {
+    return findSmallestLeaf($test_obj);
+});
+
+//Official api
+$app->get('/api/findSmallestLeaf', function (Request $request) {
+    $this->validate($request, ['tree' => 'required|json']);
+
+    // The request is valid
+    $tree = $request->input('tree');
+    $tree_obj = json_decode($tree)[0];
+    $result = findSmallestLeaf($tree_obj);
+    Log::info('JSON Tree fed in for processing: '.$tree); // Logging
+    return $result;
+});
